6547d8155975d26c026b38d9e28e74deb8da0c7f
Student8 <Student8@10.8.33.110>
2016-05-13 14:03:31 -0400

    more developments on musicplayer

--- src/mPlayer/MusicPlayer.java
+++ src/mPlayer/MusicPlayer.java
@@ -9,11 +9,22 @@ import javax.swing.*;
 public class MusicPlayer extends JFrame {
 	private JFrame frame;
 	private JPanel pages;
+	//different pages w/ content
+	private JPanel lyrics;
+	//lyrics page for cSong
+	private JPanel songSelection;
+	private JPanel categories;
+	private JLabel trackLabel;
+	private JLabel playlistLabel;
+	private JLabel albumLabel;
+	//different sorted categories
+	private JLabel cSong;
+	//current song label;
 	private JLabel controlsLabel;
+	//play/previous, next, pause
 	//http://www.tutorialspoint.com/swing/swing_jpanel.htm
 	public static void main(String[] args)	{
-		new MusicPlayer();
-		
+		MusicPlayer MusicPlayer = new MusicPlayer();
 	}
 	
 	public MusicPlayer(){
@@ -21,12 +32,30 @@ public class MusicPlayer extends JFrame {
 	}
 	
 	private void prepare(){
+		//label.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		//changes mouse pointer
 		frame = new JFrame("Music Player");
 		frame.setSize(800,800);
-		frame.setLayout(new GridLayout(2, 1, 0, 10));
+		frame.setLayout(new GridLayout(5, 1, 0, 10));
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		categories = new JPanel();
+		categories.setLayout(new FlowLayout());
+		//organized categories: Songs/Tracks, Playlist, Album, Artist(Optional)
+		trackLabel = new JLabel("Tracks", JLabel.LEFT);
+		trackLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		playlistLabel = new JLabel("Playlists");
+		playlistLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		albumLabel = new JLabel("Albums", JLabel.RIGHT);
+		albumLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		categories.add(trackLabel);
+		categories.add(playlistLabel);
+		categories.add(albumLabel);
+		frame.add(categories);
 		pages = new JPanel();
 		pages.setLayout(new FlowLayout());
+		pages.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		
+		frame.setVisible(true);
 	}
 	
 }

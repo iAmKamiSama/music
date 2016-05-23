b62eed99a22c2f5ee0effae94b8bc338002b42b0
Alexis(School) <axu4945@bths.edu>
2016-05-23 13:50:15 -0400

    MusicInterface made for Xavier

---
+++ src/mPlayer/Music.java
@@ -0,0 +1,13 @@
+package mPlayer;
+
+import java.util.ArrayList;
+
+public class Music implements MusicInterface{
+	private ArrayList<String> songNames;
+	public int[] getLyrics(String songName) {
+		// TODO Auto-generated method stub
+		int[] returnVal = new int[2];
+		return returnVal;
+	}
+	//Xavier's Class
+}
